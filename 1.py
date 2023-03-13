39565de5651ddb730c80c00c5c9b40ce6e2ff69a
han <dubisthan@gmail.com>
2023-03-14 01:22:47 +0800

    1.新增樂透邏輯 2.修改輸出樣式 3.測試完畢

--- 1.py
+++ 1.py
@@ -1,5 +1,6 @@
 import random
 
+# 樂透開獎
 numbers = random.sample(range(1, 50), 6)
-print(numbers)
-print(random.sample(range(1, 50), 1))
+print('樂透號碼: '+' '.join(map(str, sorted(numbers))))
+print(f'特別號{random.randint(1, 50)}')
