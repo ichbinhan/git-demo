662f8bdb0c04ae4366d84f591f8ca911b686df91
han <dubisthan@gmail.com>
2023-03-14 19:10:34 +0800

    1.增加勝負判斷 2.贏的時候猜了幾次

--- guess_game.py
+++ guess_game.py
@@ -1,19 +1,26 @@
 import random
 
 # 1.增加勝負判斷
-# 2.提示區間
+# 2.贏的時候是猜了幾次
+# 3.提示區間
 
 
 x = random.randint(1, 50)
+# 偷看答案
+# print(x)
 
+win = False
 for i in range(5):
-    y = eval(input('請猜一個數字(1~50之間):'))
+    y = eval(input(f'[{i+1}/5]請猜一個數字(1~50之間):'))
     if x == y:
+        win = True
         break
 
     if x > y:
         print('猜大一點')
     else:
         print('猜小一點')
-
-print(f'遊戲結束!答案為{x}')
+if win:
+    print(f'恭喜過關!總共猜了{i+1}次')
+else:
+    print(f'遊戲結束!答案為{x}')
