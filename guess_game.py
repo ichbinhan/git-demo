acb7aaf4ef66f0aab7e0f35de2ce46b99dc824f9
han <dubisthan@gmail.com>
2023-03-14 20:18:56 +0800

    新增區間提示，測試ok

--- guess_game.py
+++ guess_game.py
@@ -10,6 +10,8 @@ x = random.randint(1, 50)
 # print(x)
 
 win = False
+start, end = 1, 50
+# 總共五次可猜
 for i in range(5):
     y = eval(input(f'[{i+1}/5]請猜一個數字(1~50之間):'))
     if x == y:
@@ -17,9 +19,15 @@ for i in range(5):
         break
 
     if x > y:
-        print('猜大一點')
+        # 如果y大於原本的最小值做替換
+        if y > start:
+            start = y
+        print(f'猜大一點({start}~{end})')
     else:
-        print('猜小一點')
+        # 如果y小於原本的最小值做替換
+        if y < end:
+            end = y
+        print(f'猜小一點({start}~{end})')
 if win:
     print(f'恭喜過關!總共猜了{i+1}次')
 else:
